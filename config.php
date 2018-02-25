066bce56302f7379a254e64d46188c49db31d004
HalloRegina <36815865+HalloRegina@users.noreply.github.com>
2018-02-25 18:01:41 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+$server = 'localhost';
+$user = 'root';
+$pwd = '';
+$db = 'Filmverwaltung';
+
+try
+{
+    $con = new PDO('mysql:host='.$server.
+        ';dbname='.$db.';charset=utf8', $user, $pwd);
+} catch(Exception $e)
+{
+    switch($e->getCode())
+    {
+        case 1049:
+            echo 'Datenbank '.$db.' nicht vorhanden!<br>';
+            break;
+        default:
+            echo $e->getMessage();
+    }
+
+}
