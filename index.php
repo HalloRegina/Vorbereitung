ded5bd9f3ccc0a7936b83e77ad0e895a0955cc86
HalloRegina <36815865+HalloRegina@users.noreply.github.com>
2018-02-25 18:02:06 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include "config.php";
+?>
+<!DOCTYPE html>
+<!--
+Name: Alexander Arambasic
+Datum: 18.01.2018
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <a href="SucheProduktionsfirma.php">Suche nach Produktionsfirma</a>
+        <br>
+        <a href="SuchePerson.php">Suche nach Schauspieler</a>
+        <?php
+        ?>
+    </body>
+</html>
